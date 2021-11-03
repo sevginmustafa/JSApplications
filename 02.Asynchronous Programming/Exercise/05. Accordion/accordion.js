ffb7ff7f519f569a75886ccd3a0f45a96ab435a8
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2021-11-03 15:19:44 +0200

    ädd exercises

--- 02.Asynchronous Programming/Exercise/05. Accordion/accordion.js
+++ 02.Asynchronous Programming/Exercise/05. Accordion/accordion.js
@@ -1,3 +1,51 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    const main = document.getElementById('main');
+
+    const url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+
+    const response = await fetch(url);
+
+    const body = await response.json();
+
+    for (const title of body) {
+        const content = await getContentById(title._id);
+        const div = document.createElement('div');
+        div.classList.add('accordion');
+        div.innerHTML = `<div class="head">
+                        <span>${title.title}</span>
+                        <button class="button" id="${title._id}">More</button>
+                        </div>
+                        <div class="extra">
+                        <p>${content}</p>
+                        </div>`;
+
+        main.appendChild(div);
+    };
+
+    document.querySelectorAll('button').forEach(x => x.addEventListener('click', showMore));
 }
+
+async function getContentById(id) {
+    const url = 'http://localhost:3030/jsonstore/advanced/articles/details/' + id;
+
+    const response = await fetch(url);
+
+    const body = await response.json();
+
+    return body.content;
+}
+
+function showMore(ev) {
+    const hiddenElement = ev.target.parentNode.parentNode.querySelector('.extra');
+
+    if (hiddenElement.style.display == 'block') {
+        hiddenElement.style.display = 'none';
+        ev.target.textContent = 'More';
+    }
+    else {
+        hiddenElement.style.display = 'block';
+        ev.target.textContent = 'Less';
+    }
+}
+
+solution();
--- 02.Asynchronous Programming/Exercise/06.Blog/app.js
+++ 02.Asynchronous Programming/Exercise/06.Blog/app.js
@@ -1,5 +1,77 @@
-function attachEvents() {
-    console.log('TODO...');
+async function attachEvents() {
+    const loadPostsBtn = document.getElementById('btnLoadPosts');
+    loadPostsBtn.addEventListener('click', loadPosts);
+    
+    const viewPostBtn = document.getElementById('btnViewPost');
+    viewPostBtn.disabled = true;
+    viewPostBtn.addEventListener('click', viewPost);
 }
 
 attachEvents();
+
+async function loadPosts() {
+    const viewPostBtn = document.getElementById('btnViewPost');
+    viewPostBtn.disabled = false;
+
+    const postsSelectEl = document.getElementById('posts');
+    postsSelectEl.innerHTML = '';
+
+    const url = 'http://localhost:3030/jsonstore/blog/posts';
+
+    const response = await fetch(url);
+
+    const body = await response.json();
+
+    for (const post in body) {
+        const optionEl = document.createElement('option');
+        optionEl.value = post;
+        optionEl.textContent = body[post].title;
+
+        postsSelectEl.appendChild(optionEl);
+    }
+}
+
+async function viewPost() {
+    const postId = document.getElementById('posts').value;
+
+    const postTitleEl = document.getElementById('post-title');
+    postTitleEl.textContent = 'Loading...';
+    const postBodyEl = document.getElementById('post-body');
+    postBodyEl.textContent = '';
+    const postCommentsUlEl = document.getElementById('post-comments');
+    postCommentsUlEl.innerHTML = '';
+
+    const [post, comments] = await Promise.all([
+        await getPostById(postId),
+        await getPostCommentsById(postId)
+    ]);
+
+    postTitleEl.textContent = post.title;
+    postBodyEl.textContent = post.body;
+
+    comments.forEach(x => {
+        const liEl = document.createElement('li');
+        liEl.textContent = x.text;
+        postCommentsUlEl.appendChild(liEl);
+    });
+}
+
+async function getPostCommentsById(postId) {
+    const url = 'http://localhost:3030/jsonstore/blog/comments';
+
+    const response = await fetch(url);
+
+    const body = await response.json();
+
+    return Object.values(body).filter(x => x.postId == postId);
+}
+
+async function getPostById(id) {
+    const url = 'http://localhost:3030/jsonstore/blog/posts';
+
+    const response = await fetch(url);
+
+    const body = await response.json();
+
+    return body[id];
+}
