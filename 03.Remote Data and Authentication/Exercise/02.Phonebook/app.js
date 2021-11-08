c718f7e01fc7d17a3d9eb7a499814078954f6ddb
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2021-11-08 12:03:58 +0200

    add exercise

--- 03.Remote Data and Authentication/Exercise/02.Phonebook/app.js
+++ 03.Remote Data and Authentication/Exercise/02.Phonebook/app.js
@@ -1,5 +1,82 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoad').addEventListener('click', loadPhonebook);
+    document.getElementById('btnCreate').addEventListener('click', onCreate);
+    phonebook.addEventListener('click', onDelete);
+    loadPhonebook();
+}
+
+const phonebook = document.getElementById('phonebook');
+
+async function loadPhonebook() {
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    phonebook.replaceChildren(...Object.values(data).map(createItem));
+}
+
+async function createContact(contact) {
+    const url = 'http://localhost:3030/jsonstore/phonebook/';
+
+    const response = await fetch(url, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contact)
+    });
+
+    const result = response.json();
+
+    return result;
+}
+
+async function onCreate() {
+    const person = document.getElementById('person');
+    const phone = document.getElementById('phone');
+
+    const contact = { person: person.value, phone: phone.value };
+
+    const result = await createContact(contact);
+
+    phonebook.appendChild(createItem(result));
+
+    person.value = '';
+    phone.value = '';
+}
+
+async function deleteContact(id) {
+    const url = 'http://localhost:3030/jsonstore/phonebook/' + id;
+
+    const response = await fetch(url, {
+        method: 'delete'
+    });
+
+    const result = response.json();
+
+    return result;
+}
+
+async function onDelete(ev) {
+    const id = ev.target.dataset.id;
+
+    if (id != undefined) {
+        await deleteContact(id);
+        ev.target.parentNode.remove();
+    }
+}
+
+function createItem(contact) {
+    const li = document.createElement('li');
+    li.textContent = `${contact.person}: ${contact.phone}`;
+    const btn = document.createElement('button');
+    btn.textContent = 'Delete';
+    btn.dataset.id = contact._id;
+    li.appendChild(btn);
+
+    return li;
 }
 
 attachEvents();
--- 03.Remote Data and Authentication/Exercise/03.Students/app.js
+++ 03.Remote Data and Authentication/Exercise/03.Students/app.js
@@ -1,1 +1,66 @@
-console.log('TODO...');
+window.addEventListener('load', () => {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', onSubmit);
+
+    loadStudentsData();
+});
+
+const results = document.querySelector('#results tbody');
+
+async function loadStudentsData() {
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    results.replaceChildren(...Object.values(data).map(createItem));
+}
+
+async function createStudent(student) {
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+
+    const response = await fetch(url, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(student)
+    });
+
+    const result = await response.json();
+
+    return result;
+}
+
+async function onSubmit(ev) {
+    ev.preventDefault();
+
+    try {
+        const formData = new FormData(ev.target);
+
+        const data = [...formData.entries()].reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
+
+        if (Object.values(data).some(x => x == '')) {
+            throw new Error('All field are required')
+        }
+
+        const result = await createStudent(data);
+
+        results.appendChild(createItem(result));
+        ev.target.reset();
+    }
+    catch (error) {
+        alert(error.message);
+    }
+}
+
+function createItem(student) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${student.firstName}</td>
+                    <td>${student.lastName}</td>
+                    <td>${student.facultyNumber}</td>
+                    <td>${student.grade}</td>`;
+
+    return tr;
+}
