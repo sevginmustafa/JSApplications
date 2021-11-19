5ee8ccd9ca3b68dba2c0aea9889905a1359e7f2f
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2021-11-19 14:27:19 +0200

    add exercise

--- 05.Architecture and Testing/Exercise/02.Book-Library/test.js
+++ 05.Architecture and Testing/Exercise/02.Book-Library/test.js
@@ -24,7 +24,7 @@ function json(data) {
 }
 
 describe('Tests', async function () {
-    this.timeout(60000);
+    this.timeout(20000);
 
     let page, browser;
 
@@ -80,10 +80,28 @@ describe('Tests', async function () {
     });
 
     it('can edit book', async () => {
+        await page.goto('http://localhost:5500');
 
+        await page.click('text=Load All Books');
+
+        page.click('tbody >> text=Edit')
+
+        page.waitForSelector('form');
+
+        await page.fill('form#editForm >> input[name="title"]', 'Title 2');
+        await page.fill('form#editForm >> input[name="author"]', 'Author 2');
+
+        const [request] = await Promise.all([
+            page.waitForRequest(request => request.method() == 'PUT'),
+            page.click('form#editForm >> text=Save')
+        ]);
+
+        const data = JSON.parse(request.postData());
+        expect(data.title).to.equal('Title 2');
+        expect(data.author).to.equal('Author 2');
     });
 
-    it.only('can delete book', async () => {
+    it('can delete book', async () => {
         await page.goto('http://localhost:5500',);
 
         await page.click('text=Load All Books');
@@ -93,11 +111,10 @@ describe('Tests', async function () {
         await page.on('dialog', async dialog => await dialog.accept());
 
         const [request] = await Promise.all([
-            page.waitForRequest(request => request.method() == 'DELETE'),
+            page.waitForRequest(r => r),
             page.click('tbody >> text=Delete')
         ]);
 
-        await page.click('text=Load All Books');
-
+        expect(request.method()).to.equal('DELETE');
     });
 });
